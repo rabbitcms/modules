7809d32cf2b479613aa9a0f23a14c1a2dd4d5bad
Kulyk Dmytro <lnkvisitor.ts@gmail.com>
2017-01-26 12:58:45 +0200

    Fix seeders command replacement.

--- src/Providers/SeedServiceProvider.php
+++ src/Providers/SeedServiceProvider.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 namespace RabbitCMS\Modules\Providers;
 
 use Illuminate\Support\ServiceProvider;
@@ -8,18 +8,32 @@ use RabbitCMS\Modules\Seeders\DatabaseSeeder;
 
 /**
  * Class SeedServiceProvider.
+ *
  * @package RabbitCMS\Modules
  */
 class SeedServiceProvider extends ServiceProvider
 {
+    /**
+     * {@inheritdoc}
+     */
+    //protected $defer = true;
+
     /**
      * Register the seed console command.
      */
     public function register()
     {
         $this->app->singleton(DatabaseSeeder::class);
-        $this->app->singleton('command.seed', function ($app) {
-            return new SeedCommand($app['db']);
+        $this->app->extend('command.seed', function () {
+            return new SeedCommand($this->app['db']);
         });
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function provides()
+    {
+        return ['command.seed'];
     }
 }
