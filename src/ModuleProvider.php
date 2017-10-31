7c8dadb311eff3c7ac6d0f5310527a94f0a80237
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2017-10-31 23:48:59 +0200

    Added backward compatibility with 1.x modules.

---
+++ src/ModuleProvider.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace RabbitCMS\Modules;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class ModuleProvider
+ *
+ * @package RabbitCMS\Modules
+ * @deprecated
+ */
+class ModuleProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+    }
+}
