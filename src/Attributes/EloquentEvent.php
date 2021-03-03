e78464ab182fcbd98b529aeb030a2aee0303971f
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2021-03-03 11:33:01 +0200

    Added EloquentEvent attribute

---
+++ src/Attributes/EloquentEvent.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RabbitCMS\Modules\Attributes;
+
+use Illuminate\Support\Reflector;
+use ReflectionClass;
+use ReflectionMethod;
+
+#[\Attribute(\Attribute::TARGET_METHOD)]
+class EloquentEvent extends Event
+{
+    public const CREATING = 'creating';
+    public const CREATED = 'created';
+    public const UPDATING = 'updating';
+    public const UPDATED = 'updated';
+    public const DELETING = 'deleting';
+    public const DELETED = 'deleted';
+    public const SAVING = 'saving';
+    public const SAVED = 'saved';
+    public const RETRIEVED = 'retrieved';
+    public const REPLICATING = 'replicating';
+    public const RESTORING = 'restoring';
+    public const RESTORED = 'restored';
+    public const FORCE_DELETED = 'forceDeleted';
+
+    public function __construct(string $name, private ?string $model = null)
+    {
+        parent::__construct($name);
+    }
+
+    public function getEvent(ReflectionClass $listener, ReflectionMethod $method): ?string
+    {
+        if ($this->model === null) {
+            $this->model = Reflector::getParameterClassName($method->getParameters()[0]);
+        }
+
+        return "eloquent.{$this->name}: {$this->model}";
+    }
+}
