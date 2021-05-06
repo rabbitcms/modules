a3b576d82fa9b7bc6f86e404e4322f027270fc7a
Dmytro Kulyk <dmytro@dkulyk.name>
2021-05-07 01:13:14 +0300

    Added Fallback attribute

---
+++ src/Attributes/Fallback.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RabbitCMS\Modules\Attributes;
+
+use Attribute;
+use ReflectionClass;
+use ReflectionMethod;
+use Illuminate\Routing\{Route, Router, RouteRegistrar};
+
+#[Attribute(Attribute::TARGET_METHOD)]
+class Fallback extends RouterAttribute
+{
+    public function __invoke(ReflectionMethod|ReflectionClass $method, Router|Route|RouteRegistrar $route): Route
+    {
+        if ($route instanceof Router) {
+            return $route->fallback([$method->class, $method->name]);
+        }
+
+        return $route->fallback();
+    }
+}
